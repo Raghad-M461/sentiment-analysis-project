da93104eac198b4920d2e86fa07181a8e103cccd
Raghad-M461 <2024006846@aurak.ac.ae>
2026-06-03 13:06:43 +0400

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,18 +1,49 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+# Training dataset
 texts = [
     "The service was excellent",
-    "Terrible customer support",
     "I loved the experience",
-    "Very disappointing experience"
+    "Very professional support team",
+    "Terrible customer support",
+    "Very disappointing experience",
+    "The application crashes constantly"
 ]
 
 labels = [
     "Positive",
-    "Negative",
     "Positive",
+    "Positive",
+    "Negative",
+    "Negative",
     "Negative"
 ]
 
-print("Sentiment Analysis Dataset Loaded Successfully")
+# Convert text into numbers
+vectorizer = TfidfVectorizer()
 
-for text, label in zip(texts, labels):
-    print(text, "->", label)
+X = vectorizer.fit_transform(texts)
+
+# Train AI model
+model = LogisticRegression()
+
+model.fit(X, labels)
+
+# Test predictions
+test_texts = [
+    "The support team solved my problem quickly",
+    "The software is terrible"
+]
+
+test_vectors = vectorizer.transform(test_texts)
+
+predictions = model.predict(test_vectors)
+
+# Print predictions
+print("Sentiment Predictions:\n")
+
+for text, prediction in zip(test_texts, predictions):
+    print(text)
+    print("Prediction:", prediction)
+    print()
